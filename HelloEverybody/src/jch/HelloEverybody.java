ef09bdf47cc983d2d99089d4c6ee1125034facbe
Jorge905 <71560587+Jorge905@users.noreply.github.com>
2020-09-21 02:24:34 -0400

    First Commit

---
+++ HelloEverybody/src/jch/HelloEverybody.java
@@ -0,0 +1,26 @@
+package jch;
+
+public class HelloEverybody 
+{
+
+	public static void main(String[] args) 
+	{
+		// The code below prints text
+
+		System.out.println("Hello");
+		
+		System.out.println("Find question below");
+		
+		System.out.println("What is the capital of Canada________?");
+		
+		String answer = "Ottawa";
+		
+		System.out.print("The capital of Canada is "+answer+"!");
+		
+		
+		
+
+	
+	}
+
+}
